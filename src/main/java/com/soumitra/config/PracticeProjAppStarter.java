eac491524341599be136e469e0e182f44f63ffe7
soumitradutta <ai.soumitra@gmail.com>
2020-02-18 22:07:03 +0530

    1st

---
+++ src/main/java/com/soumitra/config/PracticeProjAppStarter.java
@@ -0,0 +1,13 @@
+package com.soumitra.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeProjAppStarter {
+	
+	public static void main(final String args[]) {
+		SpringApplication.run(PracticeProjAppStarter.class, args);
+	}
+
+}
---
+++ src/test/java/com/soumitra/PracticeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soumitra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
